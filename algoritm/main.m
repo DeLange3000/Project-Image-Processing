d73457692a333d987d96b15009c04107efcbbbda
DeLange3000 <warre@dewidewa.be>
2023-04-05 11:56:55 +0200

    added paper to repo and did initialization

---
+++ algoritm/main.m
@@ -0,0 +1,26 @@
+%% input parameters
+
+h_pixel = 50; % height of final pixelated image
+w_pixel = 20; % width of final pixelated image
+K = 5; % amount of different colors in the final image
+source_filename = "yeet"; % source file name
+
+%% open image
+
+
+
+%% algoritm
+
+%% initialize superpixels, palette and temperature
+
+%% loop for T > Tf
+
+    %% refine superpixels with modified SLIC
+
+    %% associate superpixels to colors in the palette
+
+    %% refine colors in the palette
+
+    %% check convergence of palette
+
+%% post process
